97e8fbcda4093756377d56ca1f3698769f018f5c
Mike <migueldelatorre012@gmail.com>
2023-04-06 11:56:52 +0200

    Se crea el evento click en el carrito de la compra para desplegar la lista de detalles de la compra

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamicon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const carritoCompra = document.querySelector('.navbar-shopping-cart');
+const myOrdercart = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamicon.addEventListener('click', toggleMobileMenu);
+carritoCompra.addEventListener('click', toggleOrderCart);
 
 
 
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
-}
+};
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
-}
+};
+
+function toggleOrderCart(){
+    myOrdercart.classList.toggle('inactive');
+};
 
