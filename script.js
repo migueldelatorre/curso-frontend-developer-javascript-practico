049527d5bcc139466628774bbae55e59e3f3bfeb
Mike <migueldelatorre012@gmail.com>
2023-04-06 11:18:43 +0200

    Se agregan menus en desktop y mobile al hacer click en mail y ham-menu

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuHamicon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamicon.addEventListener('click', toggleMobileMenu);
+
+
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
